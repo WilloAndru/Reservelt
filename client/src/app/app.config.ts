9eebf66fb0afda9cbbe0ed0529045c0d28ce13d0
Willoandru <wilsonandrescriollo@gmail.com>
2025-12-05 10:50:19 -0500

    Configuracion firebase fronted

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,11 +1,23 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+
+// Firebase
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+
+// Environments
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+
+    // Firebase Initialization
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+
+    // Firebase Auth
+    provideAuth(() => getAuth()),
+  ],
 };
---
+++ client/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDTul82bf0o_kYUqB3om0P2gHCVpcO6ZrA',
+    authDomain: 'reservelt-a8524.firebaseapp.com',
+    projectId: 'reservelt-a8524',
+    storageBucket: 'reservelt-a8524.firebasestorage.app',
+    messagingSenderId: '855699963206',
+    appId: '1:855699963206:web:234f1bf62a3add20f57947',
+    measurementId: 'G-T4ETB4J3R1',
+  },
+};
